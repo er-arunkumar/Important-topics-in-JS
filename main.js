5089def3352acbc917b142efad7c18ee4195237c
er-arunkumar <prasannaarun1998@gmail.com>
2022-08-19 17:42:31 +0530

    Learning Constructor and Factory functions in JS

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Object with Functions
+
 let person = {
     name:"Arun",
     age:23,
@@ -14,3 +16,43 @@ let person = {
 
 };
 person.greeting();
+
+// Factory Function - Naming Conversion using camelcase (myFirstName)
+
+function myFirstName(name){
+    return{
+    name,
+    greeting(){
+        console.log(`My name is ${this.name}`);
+    }
+};
+}
+let first=myFirstName("Arunkumar")
+first.greeting();
+
+// Construtor Function - Naming Conversion using Pascal (FirstName)
+
+function FirstName(){
+    this.name=prompt("Enter Your Name");
+    this.greeting=function() {
+        console.log(`My name is ${this.name}`);
+    }
+}
+let firstName=new FirstName()
+firstName.greeting();
+
+// Dynamic Object - Adding the Values in Dynamically and Delete the values
+
+const number={
+    name:"Arunkumar"
+}
+// Adding the values
+number.age = 24
+number.welcome =function(){
+    console.log(`My name is ${this.name}`);
+}
+
+// Delete the values
+delete number.welcome
+delete number.age
+console.log(number)
